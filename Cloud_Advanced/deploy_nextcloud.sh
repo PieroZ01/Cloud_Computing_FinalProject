29d5c21c2e0cdd36310892e6e4f5d76f79ab3114
PieroZ01 <piero.z.2001@gmail.com>
2024-08-08 18:38:33 +0200

    Added first exercise cloud advanced

---
+++ Cloud_Advanced/deploy_nextcloud.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Enter the directory where the Nextcloud files are located
+cd nextcloud
+
+# Create the nextcloud namespace
+kubectl create namespace nextcloud
+
+# Define the persistent volumes and the persistent volume claims
+# to store the data in case of a pod failure
+cd volumes # This directory contains the yaml files for the persistent volumes and the persistent volume claims
+kubectl apply -f local-path.yaml
+kubectl apply -f nextcloud-pv.yaml -n nextcloud
+kubectl apply -f nextcloud-pvc.yaml -n nextcloud
+kubectl apply -f nextcloud-postgresql-pv.yaml -n nextcloud
+kubectl apply -f nextcloud-postgresql-pvc.yaml -n nextcloud
+
+# Install the Metallb Load Balancer
+cd ../metallb # This directory contains the yaml files for the Metallb Load Balancer
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.8/config/manifests/metallb-native.yaml
+# Wait for the controller to be available
+kubectl wait --for=condition=available --timeout=600s deployment/controller -n metallb-system
+# Apply the configuration for the Metallb Load Balancer
+kubectl apply -f configmap.yaml
+kubectl apply -f ipaddresspool.yaml
+kubectl apply -f l2advertisement.yaml
+
+# Create the secrets
+cd ../secrets # This directory contains the yaml files for the secrets
+kubectl apply -f nextcloud-secrets.yaml -n nextcloud
+kubectl apply -f nextcloud-postgresql-secrets.yaml -n nextcloud
+kubectl apply -f nextcloud-redis-secrets.yaml -n nextcloud
+
+# Exit the secrets directory
+cd ..
+
+# Download the Nextcloud Helm chart and install it
+helm repo add nextcloud https://nextcloud.github.io/helm/
+helm repo update
+helm install nextcloud_advanced nextcloud/nextcloud -f values.yaml -n nextcloud
---
+++ Cloud_Advanced/initial_setup.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Load modules for container runtime
+modprobe overlay
+modprobe br_netfilter
+
+# Make the load permanent
+cat <<EOF | tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+# Change the kernel parameters
+cat <<EOF |  tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Load kernel parameters at runtime
+sysctl --system
+
+# disable zram
+touch /etc/systemd/zram-generator.conf
+swapoff -a
+
+# Disable SELinux
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Install Kubernetes
+cat << EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+# Some utils
+dnf install iproute-tc wget vim bash-completion bat -y
+# CRI-o
+dnf install crio -y
+# real kube
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# Enable and start the services
+sed -i 's/10.85.0.0\/16/10.17.0.0\/16/' /etc/cni/net.d/100-crio-bridge.conflist
+systemctl enable --now crio
+systemctl enable --now kubelet
+
+kubeadm init --pod-network-cidr=10.17.0.0/16
+
+# Copy the kubeconfig
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+alias k=kubectl
+
+# Install k9s
+dnf install wget -y
+cd /tmp
+wget https://github.com/derailed/k9s/releases/download/v0.28.2/k9s_Linux_amd64.tar.gz
+tar -xvf k9s_Linux_amd64.tar.gz
+chmod +x k9s
+mv k9s /usr/local/bin
+export EDITOR=vim
+cd ~
+
+cat << EOF | tee -a ~./basrc
+echo EDITOR=vim
+alias k=kubectl
+source < (kubectl completion bash)
+EOF
+
+# Install Helm
+dnf install -y helm
+
+# Remove the taint
+kubectl taint nodes --all  node-role.kubernetes.io/control-plane-
+
+# Create directories for the persistent volumes
+mkdir -p /home/volumes
+mkdir -p /home/volumes/nextcloud
+mkdir -p /home/volumes/postgresql
